7bbd721af0acc65b593aba2d014c0dc12c91933a
Luis Bossa <luisbossa9@gmail.com>
2026-06-03 11:27:29 -0600

    better ui

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,6 +1,126 @@
 const header = document.querySelector("header");
 const footer = document.querySelector("footer");
 
+header.innerHTML = `
+    <div class="elementor elementor-431">
+        <section
+            class="elementor-section elementor-top-section elementor-element elementor-element-49c67f1 elementor-section-boxed elementor-section-height-default elementor-section-height-default">
+            <div class="elementor-container elementor-column-gap-no">
+                <div
+                    class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-01e79b5">
+                    <div class="elementor-widget-wrap elementor-element-populated">
+                        <div
+                            class="elementor-element elementor-element-f368ed7 elementor-widget elementor-widget-image">
+                            <a href="/">
+                                <div class="elementor-widget-container">
+                                    <img width="1000" height="180" src="/images/logo.png"
+                                        class="attachment-full size-full wp-image-884" alt="Logo"
+                                        decoding="async" />
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-2bcf45b">
+                    <div class="elementor-widget-wrap elementor-element-populated">
+                        <div class="elementor-element elementor-element-8eac6ca elementor-widget elementor-widget-jkit_nav_menu"
+                            data-id="8eac6ca" data-element_type="widget"
+                            data-widget_type="jkit_nav_menu.default">
+                            <div class="elementor-widget-container">
+                                <div class="jeg-elementor-kit jkit-nav-menu break-point-tablet submenu-click-title jeg_module_5__6897a0910b7c2"
+                                    data-item-indicator="&lt;i aria-hidden=&quot;true&quot; class=&quot;fas fa-angle-down&quot;&gt;&lt;/i&gt;">
+                                    <button class="jkit-hamburger-menu">
+                                        <i aria-hidden="true" class="fas fa-bars"></i>
+                                    </button>
+                                    <div class="jkit-menu-wrapper">
+                                        <div class="jkit-menu-container">
+                                            <ul id="menu-menu-1"
+                                                class="jkit-menu jkit-menu-direction-flex jkit-submenu-position-top">
+                                                <li id="menu-item-28"
+                                                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-5 current_page_item menu-item-28">
+                                                    <a href="/index.html" aria-current="page">Home</a>
+                                                </li>
+                                                <li id="menu-item-813"
+                                                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-813">
+                                                    <a href="/#experiences">Experiences</a>
+                                                </li>
+                                                <li id="menu-item-813"
+                                                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-813">
+                                                    <a href="/#gallery">Gallery</a>
+                                                </li>
+                                                <li id="menu-item-813"
+                                                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-813">
+                                                    <a href="/#about-us">About Us</a>
+                                                </li>
+                                                <li id="menu-item-813"
+                                                    class="menu-item menu-item-type-post_type menu-item-object-page menu-item-813">
+                                                    <a href="/#testimonials">Testimonials</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <div class="jkit-nav-identity-panel">
+                                            <div class="jkit-nav-site-title">
+                                                <a href="/" class="jkit-nav-logo">
+                                                    <img src="/images/logo.png" width="90px" height="90px"
+                                                        alt="logo mobile">
+                                                </a>
+                                            </div>
+                                            <button class="jkit-close-menu">
+                                                <i aria-hidden="true" class="fas fa-times"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div class="jkit-overlay"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="elementor-column elementor-col-33 elementor-top-column elementor-element elementor-element-5582b7e elementor-hidden-tablet elementor-hidden-phone"
+                    data-id="5582b7e" data-element_type="column">
+                    <div class="elementor-widget-wrap elementor-element-populated">
+                        <div class="elementor-element elementor-element-110e379 elementor-icon-list--layout-inline elementor-align-right elementor-list-item-link-full_width elementor-widget elementor-widget-icon-list"
+                            data-id="110e379" data-element_type="widget" data-widget_type="icon-list.default">
+                            <div class="elementor-widget-container">
+
+                                <ul class="elementor-icon-list-items elementor-inline-items">
+                                    <li class="elementor-icon-list-item elementor-inline-item">
+                                        <a href="https://wa.me/50660999337" target="_blank" rel="noopener">
+                                            <span class="elementor-icon-list-icon">
+                                                <i aria-hidden="true" class="fab fa-whatsapp"></i>
+                                            </span>
+                                            <span class="elementor-icon-list-text"></span>
+                                        </a>
+                                    </li>
+                                    <li class="elementor-icon-list-item elementor-inline-item">
+                                        <a href="https://www.instagram.com/hikingguanacaste/" target="_blank"
+                                            rel="noopener">
+                                            <span class="elementor-icon-list-icon">
+                                                <i aria-hidden="true" class="fab fa-instagram"></i>
+                                            </span>
+                                            <span class="elementor-icon-list-text"></span>
+                                        </a>
+                                    </li>
+                                    <li class="elementor-icon-list-item elementor-inline-item">
+                                        <a href="https://www.facebook.com/Hikingguanacaste" target="_blank"
+                                            rel="noopener">
+                                            <span class="elementor-icon-list-icon">
+                                                <i aria-hidden="true" class="fab fa-facebook"></i>
+                                            </span>
+                                            <span class="elementor-icon-list-text"></span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+`;
+
 footer.innerHTML = `
     <div class='footer-width-fixer'>
         <div data-elementor-type="wp-post" data-elementor-id="433" class="elementor elementor-433">
@@ -214,17 +334,6 @@ footer.innerHTML = `
                                                                 <span class="elementor-icon-list-text"></span>
                                                             </a>
                                                         </li>
-                                                        <li
-                                                            class="elementor-icon-list-item elementor-inline-item">
-                                                            <a href="https://www.youtube.com/@hikingguanacaste7115"
-                                                                target="_blank" rel="noopener">
-                                                                <span class="elementor-icon-list-icon">
-                                                                    <i aria-hidden="true"
-                                                                        class="fab fa-youtube"></i>
-                                                                </span>
-                                                                <span class="elementor-icon-list-text"></span>
-                                                            </a>
-                                                        </li>
                                                     </ul>
                                                 </div>
                                             </div>
@@ -277,4 +386,3 @@ footer.innerHTML = `
         </div>
     </div>
 `;
-
